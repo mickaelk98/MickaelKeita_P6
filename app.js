1f18671d38575ee9a795057511b7628f41e6eda0
mickael keita <keitamickael@gmail.com>
2022-04-26 17:09:31 +0200

    configuration de multer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path')
 
 const userRoutes = require('./routes/user')
 
@@ -27,6 +28,7 @@ app.use((req, res, next) => {
 //* intercepte les requete de type json
 app.use(express.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/api/auth', userRoutes);
 
 
